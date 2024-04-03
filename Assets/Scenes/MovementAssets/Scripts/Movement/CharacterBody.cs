3c36b0359856239e430f4b494e0113beb2eae65a
Luciana Dughetti <ludughetti@gmail.com>
2024-04-03 13:00:10 -0300

    Added rotation drag + rigidbody rotation by AddTorque

--- Assets/Scenes/MovementAssets/Scripts/Movement/CharacterBody.cs
+++ Assets/Scenes/MovementAssets/Scripts/Movement/CharacterBody.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 [RequireComponent (typeof(Rigidbody))]
 public class CharacterBody : MonoBehaviour
 {
-    [SerializeField]
-    private float brakeMultiplier;
+    [SerializeField] private float brakeMultiplier;
+    [SerializeField] private float characterHeight;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float dragAmount;
 
     private Rigidbody _rigidbody;
     private MovementRequest _currentMovement = MovementRequest.InvalidRequest;
     private bool _isBrakeRequested = false;
+    private bool _isGrounded;
     private float _horizontalRotation = 0f;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, characterHeight * 0.5f + 0.2f, groundLayer);
     }
 
     private void Update()
     {
-        RotateCharacter();
-        MoveCharacter();
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, characterHeight * 0.5f + 0.2f, groundLayer);
+        Debug.Log($"{name}: _isGrounded {_isGrounded}");
+
+        if (_isGrounded)
+        {
+            _rigidbody.drag = dragAmount;
+        }
+        else
+        {
+            _rigidbody.drag = 0f;
+        }
     }
 
-    private void OnValidate()
+    private void FixedUpdate()
     {
-        _rigidbody = _rigidbody != null ? _rigidbody : GetComponent<Rigidbody>();
+        RotateCharacter();
+        MoveCharacter();
     }
 
     private void Reset()
@@ -62,11 +76,22 @@ public class CharacterBody : MonoBehaviour
 
         /* Multiply input.x by transform.right to move on x axis and input.y by transform.forward to move on z axis */
         Vector3 directionVector = (_currentMovement.Direction.x * transform.right + _currentMovement.Direction.z * transform.forward) * _currentMovement.Acceleration;
+        directionVector.y = 0f;
         _rigidbody.AddForce(directionVector, ForceMode.Force);
     }
 
     private void RotateCharacter()
     {
-        transform.Rotate(Vector3.up, _horizontalRotation * Time.deltaTime);
+        //transform.Rotate(Vector3.up, _horizontalRotation * Time.fixedDeltaTime);
+
+        /*if (_isBrakeRequested)
+        {
+            _rigidbody.AddTorque(-_rigidbody.velocity * brakeMultiplier, ForceMode.Impulse);
+            _isBrakeRequested = false;
+            Debug.Log($"{name}: Brake!");
+        }*/
+
+        Debug.Log($"{name}: Torque rotation is {Vector3.up * _horizontalRotation * Time.fixedDeltaTime}");
+        _rigidbody.AddTorque(Vector3.up * _horizontalRotation * Time.fixedDeltaTime, ForceMode.Impulse);
     }
 }
