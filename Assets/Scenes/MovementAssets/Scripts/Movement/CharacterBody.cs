093bac133db2f293f3f2048ced22db7ffb3c4224
Luciana Dughetti <ludughetti@gmail.com>
2024-04-21 14:34:05 -0300

    Added prefab + cleaned up movement

--- Assets/Scenes/MovementAssets/Scripts/Movement/CharacterBody.cs
+++ Assets/Scenes/MovementAssets/Scripts/Movement/CharacterBody.cs
@@ -12,7 +12,6 @@ public class CharacterBody : MonoBehaviour
     [SerializeField] private CameraControl cameraControl;
 
     [Header("In Air")]
-    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Vector3 groundedOffset = new(0f, 0.001f, 0f);
     [SerializeField] private float groundCheckDistance = 0.2f;
     [SerializeField] private float gravityScale = 3f;
@@ -20,15 +19,21 @@ public class CharacterBody : MonoBehaviour
     [SerializeField] private float jumpSpeedMultiplier = 0.75f;
 
     [Header("Movement")]
+    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float brakeMultiplier = 0.75f;
     [SerializeField] private float dragAmount = 3.5f;
     [SerializeField] private float dragAmountMultiplier = 0.1f;
+    [SerializeField] private float maxFloorDistance = 1f;
+    [SerializeField] private float maxSlopeAngle = 45;
 
     private Rigidbody _rigidbody;
     private MovementRequest _currentMovement = MovementRequest.InvalidRequest;
     private bool _isBrakeRequested = false;
     private bool _isGrounded;
+    private bool _isOnSlope;
+    private bool _isOnUnclimbableSlope;
     private float _horizontalRotation = 0f;
+    private RaycastHit _slopeHit;
     private Vector3 _rotationSpeed = Vector3.zero;
 
     private void Start()
@@ -43,23 +48,15 @@ public class CharacterBody : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Check if target is on the ground
-        _isGrounded = Physics.CheckSphere(transform.position + groundedOffset, groundCheckDistance, groundLayer);
+        UpdatePhysics();
 
-        // Adjust drag depending on if character is grounded or not
-        if (_isGrounded)
-        {
-            _rigidbody.drag = dragAmount;
-        }
-        else
-        {
-            _rigidbody.drag = 0f;
-        }
+        if (_isBrakeRequested && _isGrounded)
+            Break();
 
         RotateBody();
         MoveBody();
 
-        if (!_isGrounded)
+        if (!_isGrounded || _isOnUnclimbableSlope)
         {
             _rigidbody.AddForce((gravityScale - 1) * _rigidbody.mass * Physics.gravity);
         }
@@ -115,23 +112,63 @@ public class CharacterBody : MonoBehaviour
         return true;
     }
 
-    private void MoveBody()
+    private void UpdatePhysics()
     {
-        if (_isBrakeRequested && _isGrounded)
+        // Check if target is on the ground
+        _isGrounded = Physics.CheckSphere(transform.position + groundedOffset, groundCheckDistance, groundLayer);
+
+        // Adjust drag depending on if character is grounded or not
+        if (_isGrounded)
         {
-            _rigidbody.AddForce(-_rigidbody.velocity * brakeMultiplier, ForceMode.Impulse);
-            _isBrakeRequested = false;
-            Debug.Log($"{name}: Brake!");
+            _rigidbody.drag = dragAmount;
+        }
+        else
+        {
+            _rigidbody.drag = 0f;
         }
 
+        if (_isOnSlope && _isGrounded)
+            _rigidbody.useGravity = false;
+        else
+            _rigidbody.useGravity = true;
+    }
+
+    private void Break()
+    {
+        _rigidbody.AddForce(-_rigidbody.velocity * brakeMultiplier, ForceMode.Impulse);
+        _isBrakeRequested = false;
+        Debug.Log($"{name}: Brake!");
+    }
+
+    private void MoveBody()
+    {
         if (!_currentMovement.IsValid() || _rigidbody.velocity.magnitude >= _currentMovement.GoalSpeed)
             return;
 
         /* Multiply input.x by transform.right to move on x axis and input.y by transform.forward to move on z axis */
-        Vector3 directionVector = (_currentMovement.Direction.x * transform.right + _currentMovement.Direction.z * transform.forward) 
-            * _currentMovement.Acceleration * GetMovementDragMultiplier();
+        Vector3 directionVector = (_currentMovement.Direction.x * transform.right + _currentMovement.Direction.z * transform.forward);
+
+        if (IsOnSlope(directionVector - transform.up))
+        {
+            directionVector = Vector3.ProjectOnPlane(directionVector, _slopeHit.normal);
+        }
+
         directionVector.y = 0f;
-        _rigidbody.AddForce(directionVector, ForceMode.Force);
+
+        if(!_isOnSlope || !_isOnUnclimbableSlope)
+            _rigidbody.AddForce(directionVector * _currentMovement.Acceleration * GetMovementDragMultiplier(), ForceMode.Force);
+    }
+
+    private bool IsOnSlope(Vector3 directionVector)
+    {
+        if (Physics.Raycast(transform.position, directionVector - transform.up, out _slopeHit, maxFloorDistance, groundLayer))
+        {
+            float angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
+            _isOnSlope = angle != 0f;
+            _isOnUnclimbableSlope = angle > maxSlopeAngle;
+        }
+
+        return _isOnSlope && !_isOnUnclimbableSlope;
     }
 
     private void RotateBody()
